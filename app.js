4b624f8f758f8e33575a120c0f404cce4a7fe143
Mathews Thomas <mathewsxo@gmail.com>
2024-10-04 16:17:11 +0530

    new

--- app.js
+++ app.js
@@ -8,10 +8,24 @@ const projectRoutes = require('./routes/projectRoutes');
 dotenv.config();
 const app = express();
 
+const allowedOrigins = [
+  'https://demo-host-ten.vercel.app',
+  'https://demo-host-git-main-mathews-projects-4bf55ff3.vercel.app',
+  'https://demo-host-kaw9hbyb4-mathews-projects-4bf55ff3.vercel.app',  // Add other URLs as needed
+  'http://localhost:5173'             // Local development URL, if needed
+];
+
 app.use(cors({
-  origin: 'https://demo-host-ten.vercel.app/', // Replace with your Vercel URL
+  origin: (origin, callback) => {
+    // Allow requests with no origin (like mobile apps or Postman)
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   methods: 'GET,POST,PUT,DELETE', // Define allowed methods
-  credentials: true // Set to true if using cookies or authentication
+  credentials: true               // Enable credentials if needed
 }));
 app.use(express.json());
 
