85080f6c993101620086331f8e8bd7b882f94d80
Mathews Thomas <mathewsxo@gmail.com>
2024-10-04 15:25:38 +0530

    new

--- app.js
+++ app.js
@@ -8,7 +8,11 @@ const projectRoutes = require('./routes/projectRoutes');
 dotenv.config();
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://your-frontend.vercel.app', // Replace with your Vercel URL
+  methods: 'GET,POST,PUT,DELETE', // Define allowed methods
+  credentials: true // Set to true if using cookies or authentication
+}));
 app.use(express.json());
 
 app.use('/api/users', userRoutes);
@@ -16,10 +20,7 @@ app.use('/api/projects', projectRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => {
+mongoose.connect(process.env.MONGO_URI).then(() => {
   console.log('MongoDB connected');
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
